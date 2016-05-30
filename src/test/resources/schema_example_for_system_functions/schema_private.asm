cfc5f51c8faedc370f9458e0dc06639b3ca3b581
smcho <sm.cho@mac.com>
2016-05-30 09:26:52 -0500

    [2016/05/30]09:26AM

--- src/test/resources/schema_example_for_system_functions/schema_private.asm
+++ src/test/resources/schema_example_for_system_functions/schema_private.asm
@@ -1,38 +1,58 @@
+# This is for demonstration purposes not working code
+# The compiler generates the assembly code shown
+
 # -schema datetime = (date, time)
 # -schema location = (longitude, latitude)
 # -schema event = event | advertisement
 # -schema sender = (name, id?) | "gchat id"
 # +schema buyQuery = (sender, event, datetime, location)
 
-    function_call BUYQUERY
-    stop
+# - schema datetime
+# (a, b) => should use this template
+#
+DATETIME:
+    f _READ_TO_REG date
+    jfalsepeek DATETIME_END
+    pop $temp
+
+    f _READ_TO_REG time
+    jfalsepeek DATETIME_END
+    pop $temp
+
+    push true
+DATETIME_END:
+    return 0
+
+# event = event | advertisement
+EVENT:
+    f _READ_TO_REG_OR event advertisement 2
+    jfalsepeek EVENT_END
+    pop $temp
+EVENT_END:
+    return 0
+
+# inner function from (...)
+# -schema sender = (name, id?) | "gchat id"
+SENDER_1:
+# (name, id?)
+    f _READ_TO_REG name
+    jfalsepeek EVENT_END
+    pop $temp
+
+    f _READ_TO_REG_OPTION id
+    jfalsepeek EVENT_END
+    pop $temp
+SENDER_1_END:
+    return 0
+
+SENDER:
+    f _READ_TO_REG_OR SENDER_1 "gchat id" 2
+    jfalsepeek EVENT_END
+    pop $temp
+EVENT_END:
+    return 0
 
 # +schema buyQuery = (sender, event, datetime, location)
-BUYQUERY:
-    f SENDER
-    jpeekfalse BUYQUERYEND
-
-    f EVENT
-    jpeekfalse BUYQUERYEND
-
-    f DATETIME
-    jpeekfalse BUYQUERYEND
-
-    f LOCATION
-    jpeekfalse BUYQUERYEND
-
-    push_summary
-BUYQUERYEND:
-    r 0
-
-# -schema sender = (name, id?) | ("gchat id")
-SENDER:
-    read name
-    jpeekfalse SENDEREND
-    register name
-
-    read id
-    jpeekfalse SENDEREND
-    register id
-SENDEREND:
-    r 0
+buyQuery:
+    f _READ_TO_REG_OR_FUNCTION SENDER
+    ...
